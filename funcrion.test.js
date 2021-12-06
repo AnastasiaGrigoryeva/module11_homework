6de4356f6160f6d451f6ad89561d37c5c716291e
AnastasiaGrigoryeva <89872208+AnastasiaGrigoryeva@users.noreply.github.com>
2021-12-06 22:05:51 +0300

    2

---
+++ funcrion.test.js
@@ -0,0 +1,17 @@
+const getPercents = require('./function.test.js');
+
+describe(getPercents, ()=>{
+    const abc = 'abc';
+    const percent =  50;
+    const number = 2000;
+
+
+
+    it('Not a number', ()=>{
+        expect((abc)).toBe(0)
+    }),
+    it('Number', ()=>{
+        expect((number,percent)).toBe(1000)
+    })
+
+});
